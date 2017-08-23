a880f06abe5bcee0ea77093cbca6372a0f135054
gitnicolas <alizbaba@yahoo.com>
2017-08-23 13:04:15 +0200

    New

--- scripts/main.js
+++ scripts/main.js
@@ -12,6 +12,21 @@ var base64ToUTF8 = function (string) {
 	return decodeURI(atob(string));
 };
 
+var utf8LengthInBytes = function (string) {
+	try {
+		return (new Blob([string], {type: 'application/json'})).size;
+	} catch (error) {
+		var length = string.length;
+		for (var i = string.length - 1; i >= 0; i--) {
+			var code = string.charCodeAt(i);
+			if (code > 0x7f && code <= 0x7ff) length++;
+			else if (code > 0x7ff && code <= 0xffff) length += 2;
+			if (code >= 0xDC00 && code <= 0xDFFF) i--;
+		}
+		return length;
+	}
+};
+
 var toggleStyle = function (element, style, event) {
 	if (event) event.stopPropagation();
 	var elements = document.getElementsByClassName(element);

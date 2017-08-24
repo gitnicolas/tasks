a4d655bf6d52bfab28bf8c8368a570f46a697265
gitnicolas <alizbaba@yahoo.com>
2017-08-24 11:51:30 +0200

    New

--- scripts/main.js
+++ scripts/main.js
@@ -26,39 +26,3 @@ var utf8LengthInBytes = function (string) {
 		return length;
 	}
 };
-
-var toggleStyle = function (element, style, event) {
-	if (event) event.stopPropagation();
-	var elements = document.getElementsByClassName(element);
-	var length;
-	if (elements && (length = elements.length)) {
-		for (var i = 0; i < length; i++) {
-			var classes = elements[i].classList;
-			if (classes.contains(style)) classes.remove(style);
-			else classes.add(style);
-		}
-	}
-};
-
-var toggleNavigation = function (event) {
-	toggleStyle('page', 'expanded', event);
-};
-
-var toggleMenu = function (event) {
-	toggleStyle('menu', 'hidden', event);
-};
-
-var toggleLeftPane = function (event) {
-	toggleStyle('left-pane', 'collapsed', event);
-};
-
-var init = function () {
-	var burger = document.getElementById('burger');
-	if (burger) burger.addEventListener(tapEventName, toggleNavigation);
-
-	var action = document.getElementById('action');
-	if (action) action.addEventListener(tapEventName, toggleMenu);
-
-	var leftPane = document.getElementById('left-pane');
-	if (leftPane) leftPane.addEventListener(tapEventName, toggleLeftPane);
-};
